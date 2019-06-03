63758663e458cc05ce514abb0f427c1f88096129
didiergrand <didier.grand@scott-sports.com>
2019-06-03 12:46:24 +0200

    Change thumbnail size

--- front-page.php
+++ front-page.php
@@ -79,7 +79,7 @@ get_header();
 													 <div class="entry-img">
                            <?php
                             if ( has_post_thumbnail() ) {
-                                the_post_thumbnail();
+                                the_post_thumbnail( 'medium' );
                             }
                             else {
                                 echo '<div class="img-placeholder">nopic</div>';
