0158744165104c75b5aaba5310dbab8b4cad56ef
didiergrand <didier.grand@scott-sports.com>
2019-06-05 15:32:30 +0200

    Change Event to Events

--- front-page.php
+++ front-page.php
@@ -58,7 +58,7 @@ get_header();
 				<?php
 					$args_events = array(
 							'post_type' => 'post',
-							'category_name' => 'Event',
+							'category_name' => 'Events',
 							'posts_per_page' => 6,
 					);
 					$arr_posts_events = new WP_Query( $args_events );
