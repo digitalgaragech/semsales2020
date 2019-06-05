cdf024a5b14063d9c433a0f3cd9ea13d67931dda
didiergrand <didier.grand@scott-sports.com>
2019-06-05 15:41:31 +0200

    remove category term to title

--- functions.php
+++ functions.php
@@ -180,3 +180,24 @@ add_action( 'wp_enqueue_scripts', 'my_assets' );
 remove_filter('term_description','wpautop');
 
 add_image_size( 'square-thumb', 500, 500, true ); // Hard Crop Mode
+
+
+add_filter( 'get_the_archive_title', function ($title) {
+
+    if ( is_category() ) {
+
+            $title = single_cat_title( '', false );
+
+        } elseif ( is_tag() ) {
+
+            $title = single_tag_title( '', false );
+
+        } elseif ( is_author() ) {
+
+            $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+
+        }
+
+    return $title;
+
+});
